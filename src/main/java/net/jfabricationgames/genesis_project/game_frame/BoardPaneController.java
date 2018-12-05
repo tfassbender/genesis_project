11a18e9377ceda65f087ee3d07c4a7c0312fd363
Tobias Faßbender <tobias-fassbender@gmx.de>
2018-12-05 19:32:28 +0100

    - Added some context-menu code to the BoardPaneController to find the cell positions (that are now added to the database) - Changed the size of the background image - Fixed a bug in pom.xml

--- src/main/java/net/jfabricationgames/genesis_project/game_frame/BoardPaneController.java
+++ src/main/java/net/jfabricationgames/genesis_project/game_frame/BoardPaneController.java
@@ -15,5 +15,28 @@ public class BoardPaneController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		GuiUtils.loadImageToView("basic/background.png", true, imageViewBoardBackground);
+		//addPositionContextMenu(imageViewBoardBackground);
 	}
+	
+	/**
+	 * Used to define the positions of the field's cells.
+	 * Not used in game.
+	 */
+	/*private void addPositionContextMenu(ImageView background) {
+		ContextMenu contextMenu = new ContextMenu();
+		MenuItem printPosition = new MenuItem("Print Position");
+		printPosition.setOnAction((e2) -> {
+			Bounds boundsInScreen = background.localToScreen(background.getBoundsInLocal());
+			double backgroundXOnScreen = boundsInScreen.getMinX();
+			double backgroundYOnScreen = boundsInScreen.getMinY();
+			System.out.println(String.format("Position: %4.0f, %4.0f", contextMenu.getAnchorX() - backgroundXOnScreen,
+					contextMenu.getAnchorY() - backgroundYOnScreen));
+		});
+		contextMenu.getItems().add(printPosition);
+		background.setOnMouseClicked((e) -> {
+			if (e.getButton() == MouseButton.SECONDARY) {
+				contextMenu.show(background, e.getScreenX(), e.getScreenY());
+			}
+		});
+	}*/
 }
