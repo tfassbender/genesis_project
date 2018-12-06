d980ae4e6c945129cd3de960854854b472b4a573
Tobias Faßbender <tobias-fassbender@gmx.de>
2018-12-06 12:36:44 +0100

    - Created a pane for attacks - Implemented basic functions and fxml nodes in the AttackPaneController

--- src/main/java/net/jfabricationgames/genesis_project/game_frame/AttackPaneController.java
+++ src/main/java/net/jfabricationgames/genesis_project/game_frame/AttackPaneController.java
@@ -3,13 +3,34 @@ package net.jfabricationgames.genesis_project.game_frame;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 
 public class AttackPaneController implements Initializable {
 	
+	@FXML
+	private ImageView imageViewAttackCard;
+	
+	@FXML
+	private Label labelAttackAttacker;
+	@FXML
+	private Label labelAttackTarget;
+	@FXML
+	private Label labelAttackDefence;
+	@FXML
+	private Label labelAttackDefenceResult;
+	
+	@FXML
+	private TextArea textAreaAttackResult;
+	@FXML
+	private Button buttonAttackCommit;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
+		GuiUtils.loadImageToView("basic/attack_card.png", true, imageViewAttackCard);
 	}
 }
--- src/main/java/net/jfabricationgames/genesis_project/game_frame/PlanetInfoPaneController.java
+++ src/main/java/net/jfabricationgames/genesis_project/game_frame/PlanetInfoPaneController.java
@@ -6,35 +6,39 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class PlanetInfoPaneController implements Initializable {
-
-    @FXML
-    private Label labelPlanetInfoPosition;
-    @FXML
-    private Label labelPlanetInfoType;
-    @FXML
-    private Label labelPlanetInfoPrimariResource;
-    @FXML
-    private Label labelPlanetInfoDefence;
-    @FXML
-    private Label labelPlanetInfoAlliances;
-
-    @FXML
-    private Label labelPlanetInfoBuilding1Player;
-    @FXML
-    private Label labelPlanetInfoBuilding1Type;
-    @FXML
-    private Label labelPlanetInfoBuilding2Player;
-    @FXML
-    private Label labelPlanetInfoBuilding2Type;
-    @FXML
-    private Label labelPlanetInfoBuilding3Player;
-    @FXML
-    private Label labelPlanetInfoBuilding3Type;
-
+	
+	@FXML
+	private Label labelPlanetInfoPosition;
+	@FXML
+	private Label labelPlanetInfoType;
+	@FXML
+	private Label labelPlanetInfoPrimariResource;
+	@FXML
+	private Label labelPlanetInfoDefence;
+	@FXML
+	private Label labelPlanetInfoAlliances;
+	
+	@FXML
+	private Label labelPlanetInfoBuilding1Player;
+	@FXML
+	private Label labelPlanetInfoBuilding1Type;
+	@FXML
+	private Label labelPlanetInfoBuilding2Player;
+	@FXML
+	private Label labelPlanetInfoBuilding2Type;
+	@FXML
+	private Label labelPlanetInfoBuilding3Player;
+	@FXML
+	private Label labelPlanetInfoBuilding3Type;
+	
+	@FXML
+	private ImageView imageViewPlanet;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		GuiUtils.loadImageToView("planets/planet_center.png", true, imageViewPlanet);
 	}
 }
