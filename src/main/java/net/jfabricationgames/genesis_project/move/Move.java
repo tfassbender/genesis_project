ebf396f39f4f4b7ff1714efe19eb476c353e1f5d
Tobias Faßbender <tobias-fassbender@gmx.de>
2018-12-23 18:05:32 +0100

    - Implemented the interface IMove (not ready because alliances from the game model are missing) - Changed the name of the enum Move to MoveType

--- src/main/java/net/jfabricationgames/genesis_project/move/Move.java
+++
@@ -1,5 +0,0 @@
-package net.jfabricationgames.genesis_project.move;
-
-public enum Move {
-	
-}
---
+++ src/main/java/net/jfabricationgames/genesis_project/move/MoveType.java
@@ -0,0 +1,10 @@
+package net.jfabricationgames.genesis_project.move;
+
+public enum MoveType {
+	
+	BUILD,
+	RESEARCH,
+	RESEARCH_RESOURCES,
+	ALLIANCE,
+	PASS;
+}
